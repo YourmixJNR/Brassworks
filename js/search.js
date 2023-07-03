2ee98467ebeebfd8dad85ac31e255ccfd464764e
Michael Victor <radmakson@gmail.com>
2023-07-03 23:06:12 +0100

    give-up

--- js/search.js
+++
@@ -1,8 +0,0 @@
-const images = document.getElementsByClassName('input-img');
-
-for (let i = 0; i < images.length; i++) {
-  images[i].addEventListener('click', function () {
-    window.location = "search.html";
-  });
-}
-
