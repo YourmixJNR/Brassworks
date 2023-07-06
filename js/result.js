2bf68b27dfb6fce118fb9f08c88b7f51509eb1df
Michael Victor <radmakson@gmail.com>
2023-07-06 22:40:59 +0100

    styling the search result

--- js/result.js
+++ js/result.js
@@ -12,19 +12,28 @@ function displayResults(query, results) {
     searchResults.textContent = 'No results found.';
   } else {
     results.forEach(product => {
+      const link = document.createElement('a');
+      link.href = product.link; // Use the product-specific link
+
       const div = document.createElement('div');
       div.className = 'product'; // Add CSS class for the product container
 
-      const link = document.createElement('a');
-      link.href = product.link; // Use the product-specific link
+      const imgDiv = document.createElement('div');
+      imgDiv.className = 'product-image'; // Add CSS class for the image container
 
       const img = document.createElement('img');
       img.src = product.image;
       img.alt = '';
 
+      const detailsDiv = document.createElement('div');
+      detailsDiv.className = 'product-details'; // Add CSS class for the details container
+
       const name = document.createElement('p');
       name.className = 'product-name'; // Add CSS class for the product name
-      name.textContent = product.name;
+
+      const nameLink = document.createElement('a'); // Create the link element for the product name
+      nameLink.href = product.link; // Use the product-specific link
+      nameLink.textContent = product.name;
 
       const price = document.createElement('p');
       price.className = 'product-price'; // Add CSS class for the product price
@@ -34,15 +43,15 @@ function displayResults(query, results) {
       description.className = 'product-description'; // Add CSS class for the product description
       description.textContent = product.description;
 
-      link.appendChild(img);
-      link.appendChild(document.createElement('br'));
-      link.appendChild(name);
-      link.appendChild(document.createElement('br'));
-      link.appendChild(price);
-      link.appendChild(document.createElement('br'));
-      link.appendChild(description);
-      div.appendChild(link);
-      searchResults.appendChild(div);
+      imgDiv.appendChild(img);
+      name.appendChild(nameLink); // Wrap the product name with the link
+      detailsDiv.appendChild(name);
+      detailsDiv.appendChild(price);
+      detailsDiv.appendChild(description);
+      div.appendChild(detailsDiv);
+      div.appendChild(imgDiv);
+      link.appendChild(div); // Wrap the product container with the link
+      searchResults.appendChild(link);
     });
   }
 }
