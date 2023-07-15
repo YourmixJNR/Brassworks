8d754eba2f51dd6b6d8f7a4463f7612c684e8445
Michael Victor <radmakson@gmail.com>
2023-07-15 07:07:12 +0100

    authenticated

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,29 +1,39 @@
 //dashboard.js
 document.addEventListener('DOMContentLoaded', function () {
     //Retrieve the user object from local storage
-    const user = JSON.parse(localStorage.getItem('user'));
+    const authenticated = localStorage.getItem('authenticated');
 
-    //Retrieve the welcome container
-    const welcomeContainer = document.getElementById('welcomeContainer');
-    
-    //Check if the user object exist
-    if (user) {
-        //Display the welcome message with the username
-        welcomeContainer.textContent = 'Welcome, ' + user.username + '!';
-    } else {
+    if (authenticated !== 'true') {
         //Redirect to the sign-in page if the user is not authenticated
         window.location.href = 'signin.html';
-    }
+    } else {
+        // The user is authenticated, proceed with displaying the dashboard
 
-    // Retrieve the logout button element
-    const logoutButton = document.getElementById('logout-btn');
+        // Retrieve the user object from localStorage
+        const user = JSON.parse(localStorage.getItem('user'));
 
-    // Add an event listener to the logout button
-    logoutButton.addEventListener('click', function() {
+        //Retrieve the welcome container
+        const welcomeContainer = document.getElementById('welcomeContainer');
+
+        //Check if the user object exist
+        if (user) {
+            //Display the welcome message with the username
+            welcomeContainer.textContent = 'Welcome, ' + user.username + '!';
+        } else {
+            //Redirect to the sign-in page if the user is not authenticated
+            window.location.href = 'signin.html';
+        }
+
+        // Retrieve the logout button element
+        const logoutButton = document.getElementById('logout-btn');
+
+        // Add an event listener to the logout button
+        logoutButton.addEventListener('click', function() {
         // Remove the authentication status if needed
         localStorage.setItem('authenticated', 'false');
 
         // Redirect to the index.html page
         window.location.href = 'index.html';
-    });
+        });
+    }
 });
